62841adc65d857f53e8bc9be4beff2911d20488c
Peace89keeper <harshrawat2803@gmail.com>
2026-06-19 10:40:34 +0530

    committerd noi 7

--- goodwill/src/documents/Login.jsx
+++ goodwill/src/documents/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/user/login` {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/user/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
