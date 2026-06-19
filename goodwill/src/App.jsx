5bafad78ae20d2cc8ebacfcf90de071f6a07e345
Peace89keeper <harshrawat2803@gmail.com>
2026-06-19 10:44:51 +0530

    committed

--- goodwill/src/App.jsx
+++ goodwill/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Hero from "./documents/Hero.jsx";
 import SchemeMatchAbout from "./documents/SchemeMatchAbout.jsx";
 import SchemeMatchProfile from "./documents/SchemeMatchProfile.jsx";
 import SchemeMatchDashboard from "./documents/SchemeMatchDashboard.jsx";
 import SchemeMatchDetail from "./documents/SchemeMatchDetail.jsx";
 import SchemeMatchNotifications from "./documents/SchemeMatchNotifications.jsx";
-import AddScheme from './documents/AddScheme.jsx';
+import AddScheme from "./documents/AddScheme.jsx";
 
 import Login from "./documents/Login.jsx";
 import Register from "./documents/Resgister.jsx";
@@ -14,21 +19,21 @@ import Register from "./documents/Resgister.jsx";
 const App = () => {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/admin/add-scheme" element={<AddScheme />} />
-        <Route path="/about" element={<SchemeMatchAbout />} />
-        <Route path="/profile" element={<SchemeMatchProfile />} />
-        <Route path="/dashboard" element={<SchemeMatchDashboard />} />
-        <Route path="/detail" element={<SchemeMatchDetail />} />
-        <Route path="/notifications" element={<SchemeMatchNotifications />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-    
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/admin/add-scheme" element={<AddScheme />} />
+          <Route path="/about" element={<SchemeMatchAbout />} />
+          <Route path="/profile" element={<SchemeMatchProfile />} />
+          <Route path="/dashboard" element={<SchemeMatchDashboard />} />
+          <Route path="/detail" element={<SchemeMatchDetail />} />
+          <Route path="/notifications" element={<SchemeMatchNotifications />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/index.html" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </>
   );
 };
